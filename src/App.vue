6947ec323180be9aae8f090e396ceb6dbd7aba4a
Deceit-87 <manuelfreund318@gmail.com>
2022-03-17 03:49:11 +0100

    aggiunte bandiere con country flag

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,50 @@
 
     <input type="text"
      v-model="search"
-     placeholder="Type Here...">
-    <button @click="startSearch">Go...</button>
+     @keyup.enter="fetchData"
+     placeholder="Type Here..."
+     >
+    <button @click="fetchData"
+    >Go...</button>
 
     </div>
     
   
   <div v-for="movie in movies"
   :key="movie.id">
-  {{ movie.title }}
+ 
+        <h3>Film</h3>
+    <p>
+      Titolo: {{ movie.title }} 
+    </p>
+    <p>
+      Titolo Originale: {{ movie.original_title }}
+    </p>
+    <p>
+      Lingua: {{ getFlag( movie.original_language ) }}
+    </p>
+    <p>
+      Voto: {{ movie.vote_average }}
+    </p>
+
+  </div>
+
+  <div v-for="serie in serieTv"
+  :key="serie.id">
+ 
+        <h3>TeleFilm</h3>
+    <p>
+      Titolo: {{serie.name}}
+    </p>
+    <p>
+      Titolo Originale: {{ serie.original_name }}
+    </p>
+    <p>
+      Lingua: {{ getFlag( serie.original_language ) }}
+    </p>
+    <p>
+      Voto: {{ serie.vote_average }}
+    </p>
 
   </div>
   </div>
@@ -21,19 +56,30 @@
 <script>
 
 import axios from 'axios'
+import getUnicodeFlagIcon from 'country-flag-icons/unicode'
 
 
 export default {
   name: 'App',
   data(){
     return {
-      search:'rocco',
+      search: this.search,
       movies:[],
+      serieTv:[],
       baseUrl: 'https://api.themoviedb.org/3'
     }
   },
   methods:{
-   startSearch: function(){
+    getFlag: function(unicode){
+      if( unicode === 'en'){
+        unicode = 'gb';
+      }
+
+      return getUnicodeFlagIcon(unicode)
+
+    },
+
+   fetchData: function(){
       axios.get(`${this.baseUrl}/search/movie`,{
       params: {
         api_key: '5d4ef6c201c090629b1def178dcc3dee', 
@@ -42,12 +88,24 @@ export default {
       }
     })
     .then(res => {
-      console.log(res.data)
-      this.movies = res.data.results 
+      console.log(res.data);
+      this.movies = res.data.results ;
+      this.search = '';
+    }),
+   
+    axios.get(`${this.baseUrl}/search/tv`,{
+      params: {
+        api_key: '5d4ef6c201c090629b1def178dcc3dee', 
+        query: this.search,
+        language: 'it-IT',
+      }
     })
-   }
+    .then(res => {
+      console.log(res.data)
+      this.serieTv = res.data.results 
 
-
+    })
+   },
   },
   created(){
    
